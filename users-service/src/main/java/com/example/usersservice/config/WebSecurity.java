ca6d4e91dbaf5b8260825cc18992f0a1ba3170c2
leebongseung <yui5227@gmail.com>
2024-07-25 00:47:40 +0900

    study : web보안 설정 - SpringSecurity FilterChain을 이용하여 csrf 공격 방지 설정. - 요청 권한 설정 경로 설정, 권한 모두 허용 - 프레임 옵션 보안 헤더 비활성화, h2 콘솔로 보기위한 조치

---
+++ users-service/src/main/java/com/example/usersservice/config/WebSecurity.java
@@ -0,0 +1,32 @@
+package com.example.usersservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Web 보안 활성화
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf(AbstractHttpConfigurer::disable)
+                // 요청 권한 경로 설정하기
+                .authorizeHttpRequests(a -> {
+                    a.requestMatchers("/users/**").permitAll();
+                })
+                // 프레임 옵션 보안 헤더가 비활성화, 웹 페이지를 렌더링 해야하는 경우 필요하다!
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
+    }
+}

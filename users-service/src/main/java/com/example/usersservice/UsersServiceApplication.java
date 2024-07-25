b522c908acb9c5aa0903796e57aa136603ddcdb8
leebongseung <yui5227@gmail.com>
2024-07-25 12:26:50 +0900

    study : user-service 회원가입 완료

---
+++ users-service/src/main/java/com/example/usersservice/UsersServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.usersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}

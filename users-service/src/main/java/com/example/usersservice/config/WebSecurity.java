f5080b6346268792ec44e7683c23278e7003a245
leebongseung <yui5227@gmail.com>
2024-07-25 12:38:58 +0900

    study : h2 콘솔 인메모리로 볼 수 있게 수정

--- users-service/src/main/java/com/example/usersservice/config/WebSecurity.java
+++ users-service/src/main/java/com/example/usersservice/config/WebSecurity.java
@@ -21,7 +21,9 @@ public class WebSecurity {
                 // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
                 .csrf(AbstractHttpConfigurer::disable)
                 // 요청 권한 경로 설정하기
-                .authorizeHttpRequests(a -> a.requestMatchers("/users/**").permitAll())
+                .authorizeHttpRequests(a -> a.requestMatchers(
+                        "/users/**",
+                        "/h2-console/**").permitAll())
                 // 프레임 옵션 보안 헤더가 비활성화, 웹 페이지를 렌더링 해야하는 경우 필요하다!
                 .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
